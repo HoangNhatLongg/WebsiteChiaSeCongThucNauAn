422ff8425ade26d8bfb913b4f39a368d1fd1016d
HoangNhatLongg <162867527+HoangNhatLongg@users.noreply.github.com>
2025-10-09 22:12:30 +0700

    Navbar + Slider

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
 import './App.css';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Slider />
       <div className="container mt-5">
         <h1>Chào mừng bạn đến với CookBook 🍲</h1>
         <p>Trang web chia sẻ công thức nấu ăn bằng React + Bootstrap 5!</p>
--- front-end/src/components/Slider.jsx
+++ front-end/src/components/Slider.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import './Slider.css';
+import slider1 from '../assets/Image/Slider.jpg';
 
 export default function Slider() {
-  return <img src="" alt="" />;
+  return (
+    <div className="slider-container">
+      <img src={slider1} alt="Slider" className="slider-img" />
+      <div className="slider-text">
+        <div className="text1">Chào mừng đến với Website Nấu Ăn</div>
+        <div className="text2"> Khám phá công thức ngon và dễ làm tại nhà!</div>
+      </div>
+    </div>
+  );
 }
